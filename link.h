029f5a28c876c3c367b722a66e04be2a8b872a90
comex <comexk@gmail.com>
2011-03-16 21:33:52 -0400

    stuff

--- link.h
+++ link.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "common.h"
 #include "binary.h"
-uint32_t b_lookup_sym(const struct binary *binary, char *sym);
+uint32_t b_lookup_sym(const struct binary *binary, const char *sym, bool must_find);
 void b_relocate(struct binary *binary, const struct binary *kern, uint32_t slide);
 uint32_t b_find_sysent(const struct binary *kern);
